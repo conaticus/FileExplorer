ecf232f6ea0467c155ac2a0885797b4d143895ee
Sam <enquiry.conaticus@gmail.com>
2023-06-12 13:43:24 +0100

    add basic types

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,27 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+struct Drive {
+    name: String,
+    used_capacity: i32,
+    total_capacity: i32,
 }
+
+enum DirectoryChild {
+    File(String), // String is the path to the file
+    Directory(String)
+}
+
+// #[tauri::command]
+// fn get_drives() -> Vec<Drive> {
+// }
+
+// #[tauri::command]
+// fn open_drive() {}
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_drives])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
