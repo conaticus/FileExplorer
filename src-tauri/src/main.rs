e61380bddf085fad1fe6d82792d59d11ebdc9bcf
Sam <enquiry.conaticus@gmail.com>
2023-06-12 15:13:30 +0100

    basic get disks function (broken)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use serde::Serialize;
+use sysinfo::{DiskExt, System, SystemExt};
+
+#[derive(Serialize)]
 struct Drive {
     name: String,
-    used_capacity: i32,
-    total_capacity: i32,
+    used_capacity: u64,
+    total_capacity: u64,
 }
 
 enum DirectoryChild {
@@ -12,16 +16,27 @@ enum DirectoryChild {
     Directory(String)
 }
 
-// #[tauri::command]
-// fn get_drives() -> Vec<Drive> {
-// }
+#[tauri::command]
+fn get_disks() -> Vec<Drive> {
+    let mut disks = Vec::new();
 
-// #[tauri::command]
-// fn open_drive() {}
+    let mut sys = System::new_all();
+    sys.refresh_all();
+
+    for disk in sys.disks() {
+        let available_capacity = disk.available_space();
+        let total_capacity = disk.total_space();
+        let used_capacity = total_capacity - available_capacity;
+
+        disks.push(Drive{name: disk.name().to_string_lossy().to_string(), used_capacity, total_capacity});
+    }
+
+    disks
+}
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_drives])
+        .invoke_handler(tauri::generate_handler![get_disks])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,28 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+
+interface Disk {
+    name: string;
+    used_capacity: number;
+    total_capacity: number;
+}
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+    const [disks, setDisks] = useState<Disk[]>([]);
+    useEffect(() => {
+        async function getData() {
+            const disks = await invoke<Disk[]>("get_disks");
+            setDisks(disks);
+        }
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+        getData().catch(console.error);
+    }, [])
+
+    console.log(disks);
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
+      <div></div>
   );
 }
 
