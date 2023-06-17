757679d4d9c4943c61ad27daee87e437f7e5caa2
Sam <enquiry.conaticus@gmail.com>
2023-06-17 09:34:13 +0100

    add unversioned files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "background": "#212121",
+        "darker": "#1a1a1a",
+      }
+    },
+  },
+  plugins: [],
+}
