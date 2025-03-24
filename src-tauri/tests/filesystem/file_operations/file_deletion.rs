c04e7650d6b6fc3b01619affe9488f18e24adb8c
Brandt Marco <marco.brandt@lhind.dlh.de>
2025-03-24 13:28:13 +0100

    test changes -- working

--- src-tauri/tests/filesystem/file_operations/file_deletion.rs
+++ src-tauri/tests/filesystem/file_operations/file_deletion.rs
@@ -1,6 +1,6 @@
 
 #[test]
 fn delete_file() {
-    //permission_checker::with_permission_check("./test-data-dir/test.txt", || print_hello(5)).expect("Error running permission check");
+   // permission_checker::with_permission_check("./test-data-dir/test.txt", || print_hello(5)).expect("Error running permission check");
     
 }
